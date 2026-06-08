f56ed7a55e86d3fd2cce6124bd66488d01c03cc4
GuFurca <gfurquim05@gmail.com>
2026-06-07 21:50:11 -0300

    Add files via upload

---
+++ editais.js
@@ -0,0 +1,258 @@
+/*Variáveis */
+let editais = [];
+
+let editalSelecionado = null;
+
+let modoEdicao = false;
+
+/*Carregamento  inicial*/
+
+function carregarLocalStorage() {
+    const dados = localStorage.getItem("editais");
+    if (dados) {
+        editais = JSON.parse(dados);
+    }
+}
+
+function renderizarLista() {
+
+    const lista =
+        document.getElementById("listaEditais");
+
+    lista.innerHTML = "";
+
+    editais.forEach(edital => { /*Vai percorrer todos os editais e criar um card */
+        const card = document.createElement("div");
+
+        card.classList.add("edital");
+
+        if (edital.id === editalSelecionado) {
+            card.classList.add("ativo");
+        }
+
+        card.innerHTML = `
+            <h3>${edital.nome}</h3>
+            <p>${edital.status}</p>
+        `;
+
+        card.addEventListener("click", () => { abrirEdital(edital.id); });
+
+        lista.appendChild(card);
+    });
+}
+
+window.onload = function () { /*Quando a tela carregar, fazer essa função */
+    carregarLocalStorage();
+
+    renderizarLista();
+
+    if (editais.length > 0) {
+        abrirEdital(editais[0].id);
+    } else {
+        limparDetalhes();
+    }
+    fecharModal();
+};
+
+window.addEventListener("DOMContentLoaded", () => { /*Esse DOMContentLoaded é um evento que vai ser disparado dps que a página terminar de ser carregada */
+    document.getElementById("formEdital").addEventListener("submit", salvarEdital);
+});
+
+/*Funções referentes a tela principal (main) do editais2.js */
+function abrirEdital(id) {
+    const edital = editais.find(e => e.id === id);
+    editalSelecionado = id;
+    document.getElementById("cardDocumento").style.display = "block";
+    document.getElementById("cardCronograma").style.display = "block";
+    document.getElementById("tituloEdital").textContent = edital.nome;
+    document.getElementById("descricaoEdital").textContent = edital.descricao;
+
+    const status = document.getElementById("statusEdital");
+
+    status.textContent = edital.status;
+    status.className = "";
+
+    switch (edital.status) {
+        case "ABERTO":
+            status.classList.add("aberto");
+            break;
+
+        case "EM_ANALISE":
+            status.classList.add("analise");
+            break;
+
+        case "FINALIZADO":
+            status.classList.add("finalizado");
+            break;
+    }
+    document.getElementById("linkDocumento").href = edital.urlDocumento;
+    document.getElementById("linkDocumento").target = "_blank"; /*Esse _blank é um valor do target */
+    document.getElementById("inicioInscricao").textContent = formatarData(edital.inicioInscricao) || "-";
+    document.getElementById("fimInscricao").textContent = formatarData(edital.fimInscricao) || "-";
+    document.getElementById("resultadoPreliminar").textContent = formatarData(edital.resultadoPreliminar) || "-";
+    document.getElementById("resultadoFinal").textContent = formatarData(edital.resultadoFinal) || "-";
+
+    renderizarLista();
+}
+
+function limparDetalhes() {
+    document.getElementById("tituloEdital").textContent = "";
+    document.getElementById("descricaoEdital").textContent = "";
+    document.getElementById("statusEdital").textContent = "";
+    document.getElementById("inicioInscricao").textContent = "";
+    document.getElementById("fimInscricao").textContent = "";
+    document.getElementById("resultadoPreliminar").textContent = "";
+    document.getElementById("resultadoFinal").textContent = "";
+    document.getElementById("linkDocumento").removeAttribute("href");
+    document.getElementById("cardDocumento").style.display = "none";
+    document.getElementById("cardCronograma").style.display = "none";
+}
+
+function formatarData(data) {
+    return new Date(data).toLocaleDateString("pt-BR"); /*Uma sugestão da IA, achei bem profissional */
+}
+
+/*Funções referentes ao modal */
+function abrirModalNovo() {/*Quando for fazer um NOVO edital */
+
+    modoEdicao = false; /*isso aqui vai ser para o modal saber quando é para editar ou só fazer um novo, aqui no caso é para manter o false mesmo pq aqui não é editar*/
+
+    document.getElementById("tituloModal").textContent = "Criar Edital";
+
+    document.getElementById("formEdital").reset();
+
+    document.getElementById("modalEdital").classList.remove("oculto");
+}
+
+function abrirModalEditar() {/*Essa aqui já é quando for na vez que o usuário for editar um edital já existente */
+    if (!editalSelecionado) {
+        alert("Selecione um edital.");
+        return;
+    }
+    modoEdicao = true;
+
+    const edital = editais.find(e => e.id === editalSelecionado);
+
+    document.getElementById("tituloModal").textContent = "Editar Edital";
+
+    document.getElementById("nome").value = edital.nome;
+
+    document.getElementById("descricao").value = edital.descricao;
+
+    document.getElementById("urlDocumento").value = edital.urlDocumento;
+
+    document.getElementById("linkDownload").value = edital.linkDownload;
+
+    document.getElementById("inicioInscricaoInput").value = edital.inicioInscricao || "";
+
+    document.getElementById("fimInscricaoInput").value = edital.fimInscricao || "";
+
+    document.getElementById("resultadoPreliminarInput").value = edital.resultadoPreliminar || "";
+
+    document.getElementById("recursoInicioInput").value = edital.recursoInicio || "";
+
+    document.getElementById("recursoFimInput").value = edital.recursoFim || "";
+
+    document.getElementById("resultadoFinalInput").value = edital.resultadoFinal || "";
+
+    document.getElementById("aceitaDocumentos").checked = edital.aceitaDocumentos;
+
+    document.getElementById("status").value = edital.status;
+
+    document.getElementById("modalEdital").classList.remove("oculto");
+}
+
+function fecharModal() {
+    document.getElementById("modalEdital").classList.add("oculto");
+}
+
+function salvarEdital(event) {
+    event.preventDefault();
+    const edital = {
+        id: modoEdicao
+            ? editalSelecionado /*Equivalente a um if else */
+            : Date.now(),
+        nome: document.getElementById("nome").value,
+        descricao: document.getElementById("descricao").value,
+        urlDocumento: document.getElementById("urlDocumento").value,
+        linkDownload: document.getElementById("linkDownload").value,
+        inicioInscricao: document.getElementById("inicioInscricaoInput").value,
+        fimInscricao: document.getElementById("fimInscricaoInput").value,
+        resultadoPreliminar: document.getElementById("resultadoPreliminarInput").value || null,
+        recursoInicio: document.getElementById("recursoInicioInput").value || null,
+        recursoFim: document.getElementById("recursoFimInput").value || null,
+        resultadoFinal: document.getElementById("resultadoFinalInput").value || null,
+        aceitaDocumentos: document.getElementById("aceitaDocumentos").checked,
+        status: document.getElementById("status").value
+
+
+    };
+    /*validação da data, só impedindo de fim antes do inicio ou resultado antes do preliminar, mas tirando isso full liberdade*/
+    if (edital.fimInscricao <= edital.inicioInscricao) {
+        alert("O término das inscrições deve ser posterior ao início.");
+        return;
+    }
+
+    if (
+        edital.resultadoPreliminar &&
+        edital.resultadoFinal &&
+        edital.resultadoFinal < edital.resultadoPreliminar
+    ) {
+        alert("O resultado final não pode ser anterior ao resultado preliminar.");
+        return;
+    }
+
+    if (
+        edital.recursoInicio &&
+        edital.recursoFim &&
+        edital.recursoFim < edital.recursoInicio
+    ) {
+        alert("O fim do período de recurso deve ser posterior ao início.");
+        return;
+    }
+    /*Aqui é para ver se é de editar ou de criar um novo*/
+    if (modoEdicao) {
+        const indice =
+            editais.findIndex(
+                e => e.id === editalSelecionado
+            );
+        editais[indice] = edital;
+    } else {
+
+        editais.push(edital);
+    }
+
+    salvarLocalStorage();
+    renderizarLista();
+    abrirEdital(edital.id);
+    fecharModal();
+}
+
+function excluirEdital() {
+    if (!editalSelecionado)
+        return;
+
+    if (!confirm("Excluir edital?"))
+        return;
+
+    editais =
+        editais.filter(
+            e => e.id !== editalSelecionado
+        );
+
+    editalSelecionado = null;
+
+    salvarLocalStorage();
+    renderizarLista();
+    limparDetalhes();
+}
+
+/*Salvar o local storage*/
+function salvarLocalStorage() { /*Só comentando que isso é só para esse projeto, é que achamos meio trampo fazer o backend e dps o banco de dados para esse projeto, mas futuramente o plano é não ser o localstorage, já que ele nem funciona para diferentes usuários por ser local */
+    localStorage.setItem("editais", JSON.stringify(editais));
+}
+
+/*Para voltar para o index.html */
+function voltarMenu() {
+    window.location.href = "index.html";
+}
